23c2ba03d4a081c0019357e15e49a84725615318
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-16 21:41:15 +0530

    redis setup done

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export const login = async (req, res) => {
 
         const sessionId = crypto.randomUUID()
 
-        // redis integration
+        // redis integration done
         await redis.set(`session-${sessionId}`, JSON.stringify({
             userId: user._id,
             name: user.name,
