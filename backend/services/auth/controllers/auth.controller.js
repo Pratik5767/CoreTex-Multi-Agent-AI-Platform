77269dd7e738e04fb026ae571daf04895fa587d7
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-16 21:36:42 +0530

    implemented login and logout controllers

---
+++ backend/services/auth/controllers/auth.controller.js
@@ -0,0 +1,61 @@
+import { getAuth } from "firebase-admin/auth"
+import { app } from "../config/firebase.js"
+import userModel from "../models/user.model.js"
+import redis from "../../../shared/redis/redis.js"
+
+
+export const login = async (req, res) => {
+    try {
+        const { token } = req.body
+
+        const decoded = await getAuth(app).verifyIdToken(token)
+
+        let user = await userModel.findOne({ firebaseUID: decoded.uid })
+
+        if (!user) {
+            user = await userModel.create({
+                firebaseUID: decoded.uid,
+                name: decoded.name,
+                email: decoded.email,
+                avatar: decoded.picture
+            })
+        }
+
+        const sessionId = crypto.randomUUID()
+
+        // redis integratio
+        await redis.set(`session-${sessionId}`, JSON.stringify({
+            userId: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar
+        }), "EX", 7 * 24 * 60 * 60)
+
+        res.cookie("session", sessionId, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({ user })
+    } catch (error) {
+        return res.status(500).json({ message: `login error ${error}` })
+    }
+}
+
+
+export const logOut = async (req, res) => {
+    try {
+        const sessionId = req.cookies?.session
+
+        await redis.del(`session-${sessionId}`)
+
+        res.clearCookie("session")
+
+        return res.status(200).json({ message: "Logout successfully" })
+    } catch (error) {
+        return res.status(500).json({ message: `logout error ${error}` })
+    }
+}
+
