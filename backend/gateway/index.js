98164f536e65a880b2fa8fc6fe66011e101486bb
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-18 12:34:26 +0530

    add cookie-parser middleware to gateway service

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -3,12 +3,24 @@ import proxy from "express-http-proxy"
 import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import router from "./routes/user.routes.js"
 dotenv.config()
 
 
 const port = process.env.PORT
 
 const app = express()
+
+// middlewares
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
+app.use(cookieParser())
+
+// routes
+app.use("/api/auth", proxy(process.env.AUTH_SERVICE_URL))
+app.use("/", router)
 
 app.use("/", (req, res) => {
     res.json({ message: "Hello from Gateway" })
