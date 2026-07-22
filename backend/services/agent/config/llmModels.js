b747142a97075632c387d1ff254c4543310c02ea
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-22 21:16:06 +0530

    setup llm models

---
+++ backend/services/agent/config/llmModels.js
@@ -0,0 +1,29 @@
+import { ChatGroq } from "@langchain/groq"
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import dotenv from "dotenv"
+dotenv.config()
+
+
+const groq = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY,
+    model: "openai/gpt-oss-120b"
+})
+
+const gemini = new ChatGoogleGenerativeAI({
+    apiKey: process.env.GOOGLE_API_KEY,
+    model: "gemini-2.5-flash",
+})
+
+
+export const getModel = (agent) => {
+    switch (agent) {
+        case "chat":
+            return groq;
+        case "search":
+            return groq;
+        case "coding":
+            return gemini;
+        default:
+            return groq;
+    }
+}
