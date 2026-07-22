ee2535f93890431fc348aaa7b0950dc483fcdfea
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-22 21:23:50 +0530

    created agent controller that implement the agentic workflow with respect to db and client

---
+++ backend/services/agent/controllers/agent.controller.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { graph } from '../graph/graph.js'
+
+
+export const agentController = async (req, res) => {
+    try {
+        const { prompt, conversationId } = req.body
+
+        await axios.post(`${process.env.CHAT_SERVICE_URL}/save-message`, {
+            conversationId,
+            role: "user",
+            content: prompt
+        })
+
+        const result = await graph.invoke({ prompt, conversationId })
+        const response = result.aiResponse
+
+        // updated
+        await axios.post(`${process.env.CHAT_SERVICE_URL}/save-message`, {
+            conversationId,
+            role: "assistant",
+            content: response
+        })
+
+        return res.status(200).json(response)
+    } catch (error) {
+        return res.status(500).json({ message: `agent error ${error}` })
+    }
+}
