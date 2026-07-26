d0ce11745008514f4106d357bc9e6af2da0bfefa
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-26 23:36:35 +0530

    implemented search agent

--- backend/services/agent/agents/search.agent.js
+++ backend/services/agent/agents/search.agent.js
@@ -1,3 +1,27 @@
-export const searchAgent = async (params) => {
+import { searchTool } from "../config/tavily.js"
 
+
+export const searchAgent = async (state) => {
+    try {
+        const results = await searchTool.invoke({ query: state.prompt })
+
+        const trimmedResults = (results.results || []).map(r => ({
+            title: r.title,
+            url: r.url,
+            content: r.content?.slice(0, 200)
+        }))
+
+        return {
+            ...state,
+            searchResults: trimmedResults,
+            images: (results.images || []).splice(0, 5)
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            ...state,
+            searchResults: [],
+            images: []
+        }
+    }
 }
