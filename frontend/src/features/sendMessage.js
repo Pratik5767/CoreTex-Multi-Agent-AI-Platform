f4749c027551d8b7f7fe0b3557faf5de285b24d7
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-29 23:49:27 +0530

    created sendmessage api

---
+++ frontend/src/features/sendMessage.js
@@ -0,0 +1,15 @@
+import api from "../utils/axios"
+
+
+const sendMessage = async (payload) => {
+    try {
+        const { data } = await api.post('/api/agent/chat', payload)
+        return data
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
+
+export default sendMessage
