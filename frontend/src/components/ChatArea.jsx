c5e7ed102fe439eff916f5a3a7e41b47a9d08403
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-29 23:44:47 +0530

    updated the chatarea component

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -1,12 +1,32 @@
+import { useEffect } from 'react'
 import Nav from './Nav'
 import MessageList from './MessageList'
 import ChatInput from './ChatInput'
+import { useDispatch, useSelector } from 'react-redux'
+import getMessages from '../features/getMessages.js'
+import { setMessages } from '../redux/messageSlice.js'
 
 
 const ChatArea = () => {
 
+    const { selectedConversation } = useSelector(state => state.conversation)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const getMessage = async () => {
+            if (selectedConversation) {
+                if (selectedConversation.title == "New Chat") return;
+
+                const data = await getMessages(selectedConversation?._id)
+                dispatch(setMessages(data))
+            }
+        }
+
+        getMessage()
+    }, [selectedConversation?._id])
+
     return (
-        <div className='flex-1 flex flex-col'>
+        <div className='flex-1 flex flex-col min-w-0'>
             <Nav />
 
             <MessageList />
