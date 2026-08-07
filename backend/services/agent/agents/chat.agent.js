805e24d56ae421a1c0c7a8cecfb9c00e91ad97b9
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-08-08 00:01:53 +0530

    updated chat agent

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,11 +1,23 @@
+import { AIMessage, HumanMessage, SystemMessage } from "@langchain/core/messages"
 import { getModel } from "../config/llmModels.js"
+import { getMemory } from "../config/memory.js"
 
 
 export const chatAgent = async (state) => {
     const llm = await getModel("chat")
+    const history = await getMemory(state.conversationId)
+
+    const searchContext = state.searchResults ? `Web Search Results: ${JSON.stringify(state.searchResults)} Answer the user using only the above search results` : ""
 
     const systemPrompt = `
     You are CortexAI, an intelligent AI assistant.
+
+    ${searchContext}
+
+    If searchContext exists:
+    
+    - Use search results to answer. 
+    - Do not mention internal tools.
 
     Rules:
 
@@ -21,18 +33,25 @@ export const chatAgent = async (state) => {
     - Use fenced code blocks with language tags for code.
     - Keep paragraphs short and readable.
     - Never write headings and content on the same line.
-    - Never generate large walls of text. `
+    - Never generate large walls of text.`
 
-    const response = await llm.invoke([
-        {
-            "role": "system",
-            "content": systemPrompt
-        },
-        {
-            "role": "human",
-            "content": state.prompt
+    const messages = [
+        new SystemMessage(systemPrompt)
+    ]
+
+    history.forEach(msg => {
+        if (msg.role == "user") {
+            messages.push(new HumanMessage(msg.content))
         }
-    ])
+        if (msg.role == "assistant") {
+            messages.push(new AIMessage(msg.content))
+        }
+    });
+
+    messages.push(new HumanMessage(state.prompt))
+    console.log(messages)
+
+    const response = await llm.invoke(messages)
 
     return {
         ...state,
