384048de9246ce545f148385877499124dd61830
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-08-18 23:42:23 +0530

    modified import

--- backend/services/agent/graph/graph.js
+++ backend/services/agent/graph/graph.js
@@ -6,7 +6,7 @@ import { searchAgent } from "../agents/search.agent.js";
 import { codingAgent } from "../agents/coding.agent.js";
 import { pdfAgent } from "../agents/pdf.agent.js";
 import { pptAgent } from "../agents/ppt.agent.js";
-import { visionAgent } from "../agents/visionAgent.js";
+import { visionAgent } from "../agents/vision.agent.js";
 
 
 const workflow = new StateGraph(agentState)
