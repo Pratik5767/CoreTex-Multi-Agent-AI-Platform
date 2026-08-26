df3beb4c95b037bbab03be48d4db267603443af7
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-08-26 23:54:37 +0530

    verifypayment api done at frontend

---
+++ frontend/src/features/verifyPayment.js
@@ -0,0 +1,15 @@
+import api from "../utils/axios.js"
+
+
+const verifyPayment = async (payload) => {
+    try {
+        const { data } = await api.post('/api/billing/verify', payload)
+        return data
+    } catch (error) {
+        console.log(error)
+        return []
+    }
+}
+
+
+export default verifyPayment
