6ef74eb3c4d597dff2ec50e8c2f3d4d59b016ffd
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-08-28 22:42:33 +0530

    created api for deducting the credits for each agent usage

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -80,6 +80,7 @@ export const updateUserPayment = async (req, res) => {
         user.planExpiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
         await user.save()
 
+        // updating the session
         const sessionId = await redis.get(`user-session-${user?._id}`)
 
         await redis.set(`session-${sessionId}`, JSON.stringify({
@@ -96,5 +97,54 @@ export const updateUserPayment = async (req, res) => {
         return res.status(200).json({ success: true })
     } catch (error) {
         return res.status(500).json({ message: `update user payment error ${error}` })
+    }
+}
+
+
+export const deductCredits = async (req, res) => {
+    try {
+        const { userId, agent } = req.body
+
+        const COST = {
+            chat: 1,
+            search: 5,
+            coding: 10,
+            pdf: 10,
+            ppt: 10,
+            vision: 10
+        }
+
+        const user = await userModel.findById(userId)
+
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' })
+        }
+
+        const requiredCredits = COST[agent] || 1
+
+        if (user.credits < requiredCredits) {
+            return res.status(400).json({ message: "Not enough credits." })
+        }
+
+        user.credits -= requiredCredits
+        await user.save()
+
+        // updating the session
+        const sessionId = await redis.get(`user-session-${user?._id}`)
+
+        await redis.set(`session-${sessionId}`, JSON.stringify({
+            userId: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            plan: user.plan,
+            credits: user.credits,
+            totalCredits: user.totalCredits,
+            planExpiresAt: user.planExpiresAt
+        }), "EX", 7 * 24 * 60 * 60)
+
+        return res.status(200).json({ success: true, credits: user.credits })
+    } catch (error) {
+        return res.status(500).json({ message: `deduct credits error ${error}` })
     }
 }
