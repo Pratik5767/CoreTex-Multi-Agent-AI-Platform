cb347b594bec2bac4c315301ad0b6c428dd6140e
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-08-30 23:31:55 +0530

    updated the routes

--- backend/services/agent/routes/agent.route.js
+++ backend/services/agent/routes/agent.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { agentController } from '../controllers/agent.controller.js'
+import multer from '../config/multer.js'
 
 
 const router = express.Router()
 
-router.post('/chat', agentController)
+router.post('/chat', multer.single("file"), agentController)
 
 
 export default router
